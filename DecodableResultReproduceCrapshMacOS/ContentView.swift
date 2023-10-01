7fd54084119332ce6be398f6bf71e950bc48d784
Drew De Ponte <cyphactor@gmail.com>
2023-09-30 21:16:29 -0400

    Add code to reproduce crash

--- DecodableResultReproduceCrapshMacOS/ContentView.swift
+++ DecodableResultReproduceCrapshMacOS/ContentView.swift
@@ -7,6 +7,45 @@
 
 import SwiftUI
 
+import Foundation
+
+extension Result: Decodable where Success: Decodable, Failure == DecodingError {
+    public init(from decoder: Decoder) throws {
+        do {
+            print("DREW: in Result decoding...")
+            let container = try decoder.singleValueContainer()
+            print("DREW: got single value container")
+            let v = try container.decode(Success.self)
+            print("DREW: decoded to into variable v")
+            self = .success(v)
+        } catch let err as Failure {
+            print("DREW: failed to decode")
+            print(err)
+            self = .failure(err)
+        }
+    }
+}
+
+public struct BitbucketPaginatedResponse<T: Decodable>: Decodable {
+    public let page: Int?
+    public let size: Int?
+    public let pagelen: Int
+    public let next: String?
+    public let previous: String?
+    public let values: [T]
+}
+
+// Note: This is a non-generic version of the struct that if uncommented and substituted in
+// place of the above in the decode() call it won't crash.
+//public struct BitbucketPaginatedResponseNonGeneric: Decodable {
+//    public let page: Int?
+//    public let size: Int?
+//    public let pagelen: Int
+//    public let next: String?
+//    public let previous: String?
+//    public let values: [Result<String, DecodingError>]
+//}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -14,6 +53,30 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button {
+                let jsonString = "{\"values\": [], \"pagelen\": 10, \"size\": 0, \"page\": 1}"
+                let data = jsonString.data(using: String.Encoding.utf8)!
+
+                let formatter = DateFormatter()
+                formatter.locale = Locale(identifier: "en_US")
+                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSxxx"
+
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .formatted(formatter)
+
+                // Note: This causes the crash, EXC_BAD_ACCESS (code=1, address=0x0)
+                let responsePayload = try! decoder.decode(BitbucketPaginatedResponse<Result<String, DecodingError>>.self, from: data)
+                
+                // Note: This DOES NOT cause the crash
+//                let responsePayload = try! decoder.decode(BitbucketPaginatedResponseNonGeneric.self, from: data)
+
+
+                print("DREW: Decoded the JSON")
+                print(responsePayload)
+            } label: {
+                Text("Trigger")
+            }
+
         }
         .padding()
     }
