6024523ed735dbf3e500aacfd11ccda20cf6b6ba
Drew De Ponte <cyphactor@gmail.com>
2023-09-30 20:14:26 -0400

    Initial Commit

---
+++ DecodableResultReproduceCrapshMacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DecodableResultReproduceCrapshMacOS
+//
+//  Created by Drew De Ponte on 9/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DecodableResultReproduceCrapshMacOS/DecodableResultReproduceCrapshMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodableResultReproduceCrapshMacOSApp.swift
+//  DecodableResultReproduceCrapshMacOS
+//
+//  Created by Drew De Ponte on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DecodableResultReproduceCrapshMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
